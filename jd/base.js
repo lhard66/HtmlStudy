3a5fd3b955884165a0e15ba5db7b4abbf6ef9703
LiuTianqing <lhard@qq.com>
2016-09-30 12:05:35 +0800

    login frame

--- jd/base.js
+++ jd/base.js
@@ -5,4 +5,12 @@ window.onload = function () {
     document.getElementById('closeBanner').onclick = function () {
         document.getElementById('topBanner').style.display = 'none';
     }
+    document.getElementById('closeLogin').onclick = function () {
+        document.getElementById('login').style.display = 'none';
+        document.getElementById('cover').style.display = 'none';
+    }
+    document.getElementById('btnLogin').onclick = function () {
+        document.getElementById('login').style.display = 'block';
+        document.getElementById('cover').style.display = 'block';
+    }
 };
