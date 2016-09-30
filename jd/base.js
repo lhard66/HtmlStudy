93c83a3e1945fb7175012881b46ea609b6d6375b
LiuTianqing <lhard@qq.com>
2016-09-30 11:08:26 +0800

    ad banner close

---
+++ jd/base.js
@@ -0,0 +1,8 @@
+/**
+ * Created by HeroLiu on 09/30/2016.
+ */
+window.onload = function () {
+    document.getElementById('closeBanner').onclick = function () {
+        document.getElementById('topBanner').style.display = 'none';
+    }
+};
