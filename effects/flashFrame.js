5272ceb03c808c76be499892d17dc7016fbe8feb
LiuTianqing <lhard@qq.com>
2016-10-13 10:30:08 +0800

    flash frame javascript file

---
+++ effects/flashFrame.js
@@ -0,0 +1,33 @@
+//动态属性
+function getStyle(obj, attr) {
+    //IE
+    if (obj.currentStyle) {
+        return obj.currentStyle[attr];
+    } else {
+        //w3c标准
+        return window.getComputedStyle(obj, null)[attr];
+    }
+}
+//封闭缓动动画
+function slowMoveAnimate(obj, json) {
+    var current, step, flag;
+    obj.timer = setInterval(function () {
+        flag = true;
+        for (var attr in json) {
+            //获取当前对象的数值
+            current = parseInt(getStyle(obj, attr));
+            //计算步长=(目标-当前距离)/10
+            step = (json[attr] - current) / 10;
+            //四舍五入当前步长
+            step = step > 0 ? Math.ceil(step) : Math.floor(step);
+            obj.style[attr] = current + step + 'px';
+            if (current != json[attr]) {
+                flag = false;
+            }
+        }
+        if (flag) {
+            clearInterval(obj.timer);
+        }
+        console.log('aaa');
+    }, 30);
+}
