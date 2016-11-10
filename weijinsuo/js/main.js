b1a038bc0700cd764bbf031a26844b2dbad17c72
LiuTianqing <lhard@qq.com>
2016-11-10 14:37:05 +0800

    wjs feature part

--- weijinsuo/js/main.js
+++ weijinsuo/js/main.js
@@ -1,17 +1,33 @@
 $(function () {
-    //得到屏幕的宽度
-    //当屏幕的宽度小于768（平板）时加载小图，当屏幕的宽度大于768时加载大图
-    ////找到对应的每个节点
+    //防抖动
+    var timer = null;//用于防抖动
+    function throttle(callback, delay) {
+        clearTimeout(timer);
+        timer = setTimeout(callback, delay);
+    }
+
+//得到屏幕的宽度
+//当屏幕的宽度小于768（平板）时加载小图，当屏幕的宽度大于768时加载大图
+////找到对应的每个节点
     function fitImage() {
         var windowWidth = $(window).width();
         var isSmallScreen = windowWidth < 768;
         $('#main_banner>.carousel-inner>.item').each(function (index, item) {
             //注意item是dom对象
             var $item = $(item);
-            var fitImageUrl = isSmallScreen ? $item.data('image-xs') : $item.data('image-lg');
-            $item.css('backgroundImage', 'url(' + fitImageUrl + ')');
-
+            if (isSmallScreen) {
+                //如果是小图，则插入img标签
+                var imgHtml = '<img src="' + $item.data('image-xs') + '" alt="banner"></img>';
+                $item.empty().html(imgHtml);
+            } else {
+                //改变添加背景图片地址
+                $item.empty();
+                $item.css('backgroundImage', 'url(' + $item.data('image-lg') + ')');
+            }
         });
     }
-    $(window).on('resize',fitImage).trigger('resize');
+
+    $(window).on('resize', function () {
+        throttle(fitImage, 100);
+    }).trigger('resize');
 });
