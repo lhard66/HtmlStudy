86283edf039d0064ed043b3241a2fc8503918aca
LiuTianqing <lhard@qq.com>
2016-11-15 13:07:05 +0800

    product part scroll

--- weijinsuo/js/main.js
+++ weijinsuo/js/main.js
@@ -6,9 +6,9 @@ $(function () {
         timer = setTimeout(callback, delay);
     }
 
-//得到屏幕的宽度
-//当屏幕的宽度小于768（平板）时加载小图，当屏幕的宽度大于768时加载大图
-////找到对应的每个节点
+    //得到屏幕的宽度
+    //当屏幕的宽度小于768（平板）时加载小图，当屏幕的宽度大于768时加载大图
+    ////找到对应的每个节点
     function fitImage() {
         var windowWidth = $(window).width();
         var isSmallScreen = windowWidth < 768;
@@ -33,4 +33,16 @@ $(function () {
 
     //对tooltips进行初始化
     $('[data-toggle="tooltip"]').tooltip();
+
+    //拿到每个元素的wide，累加起来
+    var navWide = 30;
+    var navProduct = $('#nav_product');
+    navProduct.children().each(function (index, element) {
+        navWide += element.clientWidth;
+    }).parent()
+    //如果width与屏幕宽度 $(window).width() 不足时，加上滚动条，否则不加    
+    if (navWide > $(window).width()) {
+        navProduct.css('width', navWide).parent().css('overflow-x','scroll');
+    }
+    
 });
