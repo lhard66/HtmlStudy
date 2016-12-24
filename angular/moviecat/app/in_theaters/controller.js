e2aeca65c024456f91b8ba8f0792a4b8e0bb12ae
Hero <lhard@qq.com>
2016-12-24 12:05:14 +0800

    分页完成

--- angular/moviecat/app/in_theaters/controller.js
+++ angular/moviecat/app/in_theaters/controller.js
@@ -9,35 +9,55 @@
     //配置模块的路由
     moduleHot.config(['$routeProvider', function($routeProvider) {
         $routeProvider.when('/in_theaters/:page', {
-            templateUrl: 'in_theaters/view.html',
-            controller: 'InTheatersController'
+            controller: 'InTheatersController',
+            templateUrl: 'in_theaters/view.html'
         });
     }]);
 
     moduleHot.controller('InTheatersController', [
         '$scope',
-        '$http', 
+        '$route',
+        '$routeParams',
+        // '$http',
         'HttpService',
-        function($scope,$http,HttpService) {    	
-        $scope.title = '正在热映';
-        $scope.data=[];
-        $scope.msg='';
-        // 1.使用不跨域的方便请求数据
-        // $http.get('data.json').then(function(response){
-        //     $scope.data=response.data;
-        // },function(err){
-        //     $scope.msg='请求异常，请稍候再试。';            
-        // });
+        function($scope, $route, $routeParams, HttpService) { //这里删除了$http形参
+            $scope.loading = true;
+            $scope.title = '正在热映'; //标题
+            $scope.data = []; //拿到的豆瓣数据
+            $scope.msg = ''; //错误信息
+            $scope.pageCount = 3; //一页显示多少条
+            $scope.currentPage = parseInt($routeParams.page); //
+            $scope.start = ($scope.currentPage - 1) * $scope.pageCount; //从那里开始读取数据
+            $scope.totalPages = 0;
+            // 1.使用不跨域的方便请求数据
+            // $http.get('data.json').then(function(response){
+            //     $scope.data=response.data;
+            // },function(err){
+            //     $scope.msg='请求异常，请稍候再试。';            
+            // });
 
-        //2.使用自定义的跨域请求
-        HttpService.jsonp(
-            'http://api.douban.com/v2/movie/in_theaters', 
-            { start: 3, count: 8 },
-            function(data){
-                $scope.data=data;
-                $scope.$apply();
-        });
-    }])
+            //2.使用自定义的跨域请求
+            HttpService.jsonp(
+                'http://api.douban.com/v2/movie/in_theaters', { start: $scope.start, count: $scope.pageCount },
+                function(data) {
+                    $scope.data = data;
+                    $scope.title = data.title;
+                    $scope.totalPages = Math.ceil(data.total / $scope.pageCount);//共多少页
+                    console.log($scope.totalPages);
+                    $scope.loading = false;
+                    $scope.$apply();
+                });
+            $scope.goPage = function(page) {
+                console.log(page);
+
+                if (page > 0 && page <= $scope.totalPages) {
+                    console.log(666);
+
+                    $route.updateParams({ page: page });
+                }
+            }
+        }
+    ]);
 
 
 })(angular);
